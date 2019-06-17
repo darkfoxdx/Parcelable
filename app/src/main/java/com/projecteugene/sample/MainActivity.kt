07c60ce63490aacb9c57b299abe1121834bb2f81
Eugene Low <eugenelow@omesti.com>
2019-06-17 12:30:55 +0800

    First commit

---
+++ app/src/main/java/com/projecteugene/sample/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.projecteugene.sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.projecteugene.sample.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
